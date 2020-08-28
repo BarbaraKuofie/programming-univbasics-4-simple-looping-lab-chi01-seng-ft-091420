f64b50d678850abbb080fa1204bb0b54e75834b2
Barbara Kuofie <barbara.kuofie@gmail.com>
2020-08-28 03:26:49 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -6,18 +6,26 @@ def loop_message_five_times (message)
     count +=1
   end 
 end 
+
+def loop_message_n_times (message, limit)
+  count = 0 
+  while count < limit do 
+    puts message
+    count += 1 
+  end 
+end 
   
 
 def outpt_array (array)
   counter = 0
-  while counter <= array.length do 
+  while counter < array.length do 
     puts array[counter]
     counter += 1
   end 
 end 
 
 def return_string_array (array)
-  while array(i) <= array.length do
+  while array(i) < array.length do
     return array.to_s
   end
 end 
